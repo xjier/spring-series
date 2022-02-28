cfc909012098df4dd44a859d8718fd050d983b69
jier <axjier@163.com>
2022-02-28 22:06:49 +0800

    add files

---
+++ lesson-001/src/main/java/com/javacode/lesson001/demo1/Client.java
@@ -0,0 +1,18 @@
+package com.javacode.lesson001.demo1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+
+    public static void main(String[] args) {
+        //1.bean配置文件位置
+        String beanXml = "classpath:/com/javacode/lesson001/demo1/bean.xml";
+        //String beanXml = "classpath:/beans.xml";
+        //2.创建ClassPathXmlApplicationContext容器，给容器指定需要加载的bean配置文件
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(beanXml);
+        //3.从容器中获取需要的bean
+        HelloWorld helloWorld = context.getBean("helloWorld", HelloWorld.class);
+        //4.使用对象
+        helloWorld.say();
+    }
+}
---
+++ lesson-001/src/main/java/com/javacode/lesson001/demo1/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.javacode.lesson001.demo1;
+
+public class HelloWorld {
+    public void say() {
+        System.out.println("hello,一起学spring!");
+    }
+}
